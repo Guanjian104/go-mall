d8e5275f1e7d699a2ce2ba89b9ff9ac78db547b7
gj <gl451845102@163.com>
2025-10-21 18:43:37 +0800

    增加请求访问、响应日志中间件和全局Panic日志记录中间件

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
     logger.Init()
 
-    g.Use(gin.Logger(), middleware.StartTrace())
+    g.Use(gin.Logger(), middleware.StartTrace(), middleware.LogAccess(), middleware.GinPanicRecovery())
     g.GET("/ping", func(c *gin.Context) {
         c.JSON(http.StatusOK, gin.H{
             "message": "pong",
@@ -35,6 +35,22 @@ func main() {
         logger.Info(c, "logger test", "key", "keyName", "val", 3)
         c.JSON(http.StatusOK, gin.H{
             "status": "ok",
+        })
+    })
+
+    g.POST("/access-log-test", func(c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{
+            "status": "ok",
+        })
+    })
+
+    g.GET("/panic-log-test", func(c *gin.Context) {
+        var a map[string]string
+        // TODO: panic 测试接口，需要的时候在这里 Panic 一下
+        // a["k"] = "v"
+        c.JSON(http.StatusOK, gin.H{
+            "status": "ok",
+            "data": a,
         })
     })
 
